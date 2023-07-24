f760e18027b3a234d4c64afd7a5d223f76d04908
Thingital <116533709+iot288@users.noreply.github.com>
2023-07-24 15:42:33 -0400

    Update Metacity.sol

--- Metacity.sol
+++ Metacity.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+//
+contract Metacity is ERC721URIStorage, Ownable {
+
+    constructor() ERC721("Metacity", "mct") {}
+//
+    function mint(
+        address to, 
+        uint256 tokenId, 
+        string calldata _uri
+        ) external onlyOwner {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _uri);
+    }
+}
